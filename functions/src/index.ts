9fd85db40aa44568100d48e07d1c17d850380a70
Alex Knowlton <alexrules313@gmail.com>
2019-04-19 19:01:34 -0400

    added image hook

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Firebase
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+// Cloud Vision
+import * as vision from '@google-cloud/vision';
+const visionClient =  new vision.ImageAnnotatorClient();
+
+// Dedicated bucket for cloud function invocation
+const bucketName = 'cloud-computing-235700-vision';
+
+export const imageTagger = functions.storage
+    .bucket(bucketName)
+    .object()
+    .onChange(async event => {
+            const object = event.data;
+            const filePath = object.name;   
+
+            // Location of saved file in bucket
+            const imageUri = `gs://${bucketName}/${filePath}`;
+
+            const docId = filePath.split('.jpg')[0];
+
+            const docRef  = admin.firestore().collection('photos').doc(docId);
+
+            // Await the cloud vision response
+            const results = await visionClient.faceDetection(imageUri);
+            const faces = results.faceAnnotations;
+
+            return docRef.set(faces)           
+});

906a7eb3c95fce4c6f6bac121f39e4567eacf4f7
Alex Knowlton <alexrules313@gmail.com>
2019-04-19 16:54:11 -0400

    user can no longer take photo before camera is ready

--- vision/src/app/home/home.page.ts
+++ vision/src/app/home/home.page.ts
@@ -7,73 +7,69 @@ import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, Camer
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private cameraPreview: CameraPreview) { 
+  constructor(private cameraPreview: CameraPreview) { }
+  cameraStarted = false
 
-    // // Set the handler to run every time we take a picture
-    // this.cameraPreview.setOnPictureTakenHandler().subscribe((result) => {
-      //   console.log(result);
-      //   // do something with the result
-      // });
-    }
-
-    ionViewDidEnter() {
-    	this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
-        (res) => {
-          console.log(res)
-          console.log("started!!!")
-        },
-        (err) => {
-          console.log(err)
-          console.log('ripppppp :(')
-        });
-    }
-
-    // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
-    cameraPreviewOpts: CameraPreviewOptions = {
-      x: 0,
-      y: 0,
-      width: window.screen.width,
-      height: window.screen.height,
-      camera: 'rear',
-      tapPhoto: true,
-      previewDrag: true,
-      toBack: true,
-      alpha: 1
-    }
-
-
-    // picture options
-    pictureOpts: CameraPreviewPictureOptions = {
-      width: 1280,
-      height: 1280,
-      quality: 85
-    }
-
-    // placeholder for photo
-    picture = '';
-
-    // take a picture
-    takePicture() {
-      this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
-        this.picture = 'data:image/jpeg;base64,' + imageData;
-      }, (err) => {
-        console.log(err);
-        this.picture = 'assets/img/test.jpg';
+  ionViewDidEnter() {
+    this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
+      (res) => {
+        this.cameraStarted = true
+        console.log(res)
+        console.log("started!!!")
+      },
+      (err) => {
+        this.cameraStarted = true
+        console.log(err)
+        console.log('ripppppp :(')
       });
-    }
-
-    // Switch camera
-    switchCamera() {
-      this.cameraPreview.switchCamera();
-    }
-
-    // set color effect to negative
-    setColorEffect() {
-      this.cameraPreview.setColorEffect('negative');
-    }
-
-    // Stop the camera preview
-    stopCamera() {
-      this.cameraPreview.stopCamera();
-    }
   }
+
+  // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
+  cameraPreviewOpts: CameraPreviewOptions = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: 'rear',
+    tapPhoto: true,
+    previewDrag: true,
+    toBack: true,
+    alpha: 1
+  }
+
+
+  // picture options
+  pictureOpts: CameraPreviewPictureOptions = {
+    width: 1280,
+    height: 1280,
+    quality: 85
+  }
+
+  // placeholder for photo
+  picture = '';
+
+  // take a picture
+  takePicture() {
+    this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+      this.picture = 'assets/img/test.jpg';
+    });
+  }
+
+  // Switch camera
+  switchCamera() {
+    this.cameraPreview.switchCamera();
+  }
+
+  // set color effect to negative
+  setColorEffect() {
+    this.cameraPreview.setColorEffect('negative');
+  }
+
+  // Stop the camera preview
+  stopCamera() {
+    this.cameraPreview.stopCamera();
+  }
+}

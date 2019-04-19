720bb30e677c3aa3dc1e805ac3834350869af784
Alex Knowlton <alexrules313@gmail.com>
2019-04-19 16:29:51 -0400

    moved cameraStart out of constructor

--- vision/src/app/home/home.page.ts
+++ vision/src/app/home/home.page.ts
@@ -9,8 +9,15 @@ import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, Camer
 export class HomePage {
   constructor(private cameraPreview: CameraPreview) { 
 
+    // // Set the handler to run every time we take a picture
+    // this.cameraPreview.setOnPictureTakenHandler().subscribe((result) => {
+      //   console.log(result);
+      //   // do something with the result
+      // });
+    }
+
     // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
-    const cameraPreviewOpts: CameraPreviewOptions = {
+    cameraPreviewOpts: CameraPreviewOptions = {
       x: 0,
       y: 0,
       width: window.screen.width,
@@ -23,54 +30,51 @@ export class HomePage {
     }
 
     // start camera
-    this.cameraPreview.startCamera(cameraPreviewOpts).then(
-      (res) => {
-        console.log(res)
-      },
-      (err) => {
-        console.log(err)
+    startCamera() {
+      this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
+        (res) => {
+          console.log(res)
+          console.log("started!!!")
+        },
+        (err) => {
+          console.log(err)
+          console.log('ripppppp :(')
+        });
+    }
+
+
+    // picture options
+    pictureOpts: CameraPreviewPictureOptions = {
+      width: 1280,
+      height: 1280,
+      quality: 85
+    }
+
+    // placeholder for photo
+    picture = '';
+
+    // take a picture
+    takePicture() {
+      this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+        this.picture = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        console.log(err);
+        this.picture = 'assets/img/test.jpg';
       });
+    }
 
-    // // Set the handler to run every time we take a picture
-    // this.cameraPreview.setOnPictureTakenHandler().subscribe((result) => {
-    //   console.log(result);
-    //   // do something with the result
-    // });
+    // Switch camera
+    switchCamera() {
+      this.cameraPreview.switchCamera();
+    }
+
+    // set color effect to negative
+    setColorEffect() {
+      this.cameraPreview.setColorEffect('negative');
+    }
+
+    // Stop the camera preview
+    stopCamera() {
+      this.cameraPreview.stopCamera();
+    }
   }
-
-
-  // picture options
-  pictureOpts: CameraPreviewPictureOptions = {
-    width: 1280,
-    height: 1280,
-    quality: 85
-  }
-
-  // placeholder for photo
-  picture = '';
-  
-  // take a picture
-  takePicture() {
-    this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
-      this.picture = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      console.log(err);
-      this.picture = 'assets/img/test.jpg';
-    });
-  }
-
-  // Switch camera
-  switchCamera() {
-    this.cameraPreview.switchCamera();
-  }
-
-  // set color effect to negative
-  setColorEffect() {
-    this.cameraPreview.setColorEffect('negative');
-  }
-
-  // Stop the camera preview
-  stopCamera() {
-    this.cameraPreview.stopCamera();
-  }
-}
